005b0ae296b5b82729f0a0e8dc4c0458789ea58f
Injae <hwick@nate.com>
2020-09-08 22:26:15 +0900

    Initial commit

---
+++ src/main/java/com/stackstock/stackstock/StackstockApplication.java
@@ -0,0 +1,12 @@
+package com.stackstock.stackstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackstockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StackstockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stackstock/stackstock/StackstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackstock.stackstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackstockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

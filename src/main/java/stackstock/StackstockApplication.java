e00126bbbda7b3d3fc2116206ee7c29c7e73f497
youngjunShin <201211211yj@gmail.com>
2021-01-20 23:05:12 +0900

    Service 구현

--- src/main/java/stackstock/StackstockApplication.java
+++ src/main/java/stackstock/StackstockApplication.java
@@ -1,12 +1,29 @@
 package stackstock;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import stackstock.service.GetKOSPIDataService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
-public class StackstockApplication {
+public class StackstockApplication implements CommandLineRunner {
+    @Autowired
+    GetKOSPIDataService getKOSPIDataService;
+
     public static void main(String[] args) {
         SpringApplication.run(StackstockApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        List<String> codes = new ArrayList<>();
+
+        codes.add("005930");
+        codes.add("000660");
+        getKOSPIDataService.fetchKospiPages(codes);
+    }
 }
--- src/main/java/stackstock/service/GetKOSPIDataService.java
+++ src/main/java/stackstock/service/GetKOSPIDataService.java
@@ -1,16 +1,68 @@
 package stackstock.service;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.List;
 
 @Service
 public class GetKOSPIDataService {
     @Autowired
     WebClient webClient;
 
-    void doSometing(){
+    public void getData(){
+        Mono<String> result = webClient.get().
+                uri("https://finance.naver.com/item/main.nhn?code=005930").
+                retrieve().
+                bodyToMono(String.class);
+
+        result.subscribe(i->{
+            Document doc = Jsoup.parse(i);
+            Elements elements = doc.getElementById("middle").
+                    getElementsByClass("blind").
+                    first().
+                    getElementsByTag("dd");
+            for(Element element : elements){
+                System.out.println(element.text());
+            }
+
+        });
+    }
+
+    Mono <String> getKospiPageByCode(String code){
+        return webClient.get().
+                uri("https://finance.naver.com/item/main.nhn?code={code}", code).
+                retrieve().
+                bodyToMono(String.class);
+    }
+
+    public void fetchKospiPages(List<String> codes) {
+        Flux <String> result = Flux.fromIterable(codes)
+                        .parallel()
+                        .runOn(Schedulers.elastic())
+                        .flatMap(this::getKospiPageByCode)
+                        .ordered((s1, s2) -> s1.length() - s2.length());
+
+        result.subscribe(i->{
+            Document doc = Jsoup.parse(i);
+            Elements elements = doc.getElementById("middle").
+                    getElementsByClass("blind").
+                    first().
+                    getElementsByTag("dd");
+            for(Element element : elements){
+                System.out.println(element.text());
+            }
+            System.out.println();
+        });
     }
 
     @Bean
